87213ee23c0c95cb630b12cde364bda93643d645
HovhannesSisoyan <hovosisoyan@gmail.com>
2019-09-30 22:25:25 +0400

    improved version

--- copyFilesWithProcesses.py
+++ copyFilesWithProcesses.py
@@ -2,16 +2,15 @@ from concurrent.futures import ProcessPoolExecutor
 import os
 
 def createFile(root, file, pathT):
-    os.system('dd if={} of={}'.format(os.path.join(root, file), os.path.join(pathT, file)))
+    os.system('dd if={} of={}  count=1024000 bs=1024'.format(os.path.join(root, file), os.path.join(pathT, file)))
 
 
 pathFrom = '/home/hovhannes/copyFrom/'
 pathTo = '/home/hovhannes/copyTo/'
 
 # r=root, d=directories, f = files
-executor = ProcessPoolExecutor(100);
-
+executor = ProcessPoolExecutor(max_workers=8);
 for r, d, f in os.walk(pathFrom):
     for file in f:
-        feature = executor.submit(createFile(r, file, pathTo))
+        executor.submit(createFile(r, file, pathTo))
 
--- copyFilesWithoutMultithreading.py
+++ copyFilesWithoutMultithreading.py
@@ -1,15 +1,14 @@
 import os
 
 def createFile(root, file, pathT):
-    os.system('dd if={} of={}'.format(os.path.join(root, file), os.path.join(pathT, file)))
+    os.system('dd if={} of={} count=1024000 bs=1024'.format(os.path.join(root, file), os.path.join(pathT, file)))
 
 
 pathFrom = "/home/hovhannes/copyFrom/"
 pathTo = "/home/hovhannes/copyTo/"
 
 # r=root, d=directories, f = files
-
-for r, d, f in os.walk(pathFrom):
+sfor r, d, f in os.walk(pathFrom):
     for file in f:
         createFile(r, file, pathTo)
 
